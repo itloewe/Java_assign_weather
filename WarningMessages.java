4d1579e80b2ebd35dc8e3760ff96f369cf51b2af
itloewe <45351388+itloewe@users.noreply.github.com>
2020-11-16 11:36:28 +0100

    Update WarningMessages.java

--- WarningMessages.java
+++ WarningMessages.java
@@ -1,9 +1,1 @@
 
-public interface WarningMessages {
-
-	public static final String NO_WARNING = "no Warning";
-	public static final String COLD_WARNING = "Cold Warning!";
-	public static final String WIND_WARNING = "Wind Warning";
-	public static final String STORM_WARNING = "Storm and Cold Warning!";
-	
-}
